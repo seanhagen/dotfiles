c378fcc3a7bf6c6cbcc217ce0db57ae043665bbb
Sean Patrick Hagen <github@seanhagen.ca>
2014-03-25 22:07:34 -0700

    Updating emacs config

--- shell/bin/update-email.sh
+++ shell/bin/update-email.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-eval \`gnome-keyring-daemon\`
-export GNOME_KEYRING_PID
-export GNOME_KEYRING_SOCKET
+source $HOME/.Xdbus
 /usr/bin/offlineimap -o -u ttyui #-a Gmail
 /usr/local/bin/mu index -m /home/sean/Mail
