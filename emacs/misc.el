101109a96c76bc27670f49f09c04d2b692a9a2b6
Sean Patrick Hagen <shagen@bardel.ca>
2015-01-13 18:09:49 -0800

    updating more!

--- emacs/misc.el
+++ emacs/misc.el
@@ -73,24 +73,24 @@
              [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
   (global-unset-key k))
 
-(defvar flymake-fringe-overlays nil)
-(make-variable-buffer-local 'flymake-fringe-overlays)
+;; (defvar flymake-fringe-overlays nil)
+;; (make-variable-buffer-local 'flymake-fringe-overlays)
 
-(defadvice flymake-make-overlay (after add-to-fringe first
-                                 (beg end tooltip-text face mouse-face)
-                                 activate compile)
-  (push (fringe-helper-insert-region
-         beg end
-         (fringe-lib-load (if (eq face 'flymake-errline)
-                              fringe-lib-exclamation-mark
-                            fringe-lib-question-mark))
-         'left-fringe 'font-lock-warning-face)
-        flymake-fringe-overlays))
+;; (defadvice flymake-make-overlay (after add-to-fringe first
+;;                                  (beg end tooltip-text face mouse-face)
+;;                                  activate compile)
+;;   (push (fringe-helper-insert-region
+;;          beg end
+;;          (fringe-lib-load (if (eq face 'flymake-errline)
+;;                               fringe-lib-exclamation-mark
+;;                             fringe-lib-question-mark))
+;;          'left-fringe 'font-lock-warning-face)
+;;         flymake-fringe-overlays))
 
-(defadvice flymake-delete-own-overlays (after remove-from-fringe activate
-                                        compile)
-  (mapc 'fringe-helper-remove flymake-fringe-overlays)
-  (setq flymake-fringe-overlays nil))
+;; (defadvice flymake-delete-own-overlays (after remove-from-fringe activate
+;;                                         compile)
+;;   (mapc 'fringe-helper-remove flymake-fringe-overlays)
+;;   (setq flymake-fringe-overlays nil))
 
 (autoload 'ack-same "full-ack" nil t)
 (autoload 'ack "full-ack" nil t)
