eab3165761e61831022a3fca7556e4b6876499e8
Sean Patrick Hagen <sean.hagen@gmail.com>
2015-08-14 23:27:22 -0700

    removing commented out stuff

--- emacs/modes/jabber-customize.el
+++ emacs/modes/jabber-customize.el
@@ -1,7 +1,5 @@
 (require 'jabber)
 (require 'netrc)
-;(require 'autosmiley)
-;; (add-hook 'jabber-chat-mode-hook 'autosmiley-mode)
 
 ;; (defvar hipchat-number "182296")
 ;; (defvar hipchat-nickname "Sean Hagen")
@@ -77,40 +75,5 @@
 (define-key jabber-chat-mode-map (kbd "RET") 'newline)
 (define-key jabber-chat-mode-map [C-return] 'jabber-chat-buffer-send)
 (define-key jabber-chat-mode-map [escape] 'my-jabber-chat-delete-or-bury)
-
-;; (define-key mode-specific-map "jr"
-;;   (lambda ()
-;;     (interactive)
-;;     (switch-to-buffer "*-jabber-*")))
-
-;; (define-key mode-specific-map "jc"
-;;   '(lambda ()
-;;      (interactive)
-;;      (call-interactively 'jabber-connect)))
-
-;; (define-key mode-specific-map "jd"
-;;   '(lambda ()
-;;      (interactive)
-;;      (call-interactively 'jabber-disconnect)))
-
-;; (define-key mode-specific-map "jj"
-;;   '(lambda ()
-;;      (interactive)
-;;      (call-interactively 'jabber-chat-with)))
-
-;; (define-key mode-specific-map "ja"
-;;   '(lambda ()
-;;      (interactive)
-;;      (jabber-send-presence "away" "" 10)))
-
-;; (define-key mode-specific-map "jo"
-;;   '(lambda ()
-;;      (interactive)
-;;      (jabber-send-presence "" "" 10)))
-
-;; (define-key mode-specific-map "jx"
-;;   '(lambda ()
-;;      (interactive)
-;;      (jabber-send-presence "xa" "" 10)))
 
 (provide 'jabber-customize)
