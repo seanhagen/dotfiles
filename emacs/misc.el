5b85cdabd32275cb1af78d99d3d50ff1ac7b10f1
Sean Patrick Hagen <shagen@bardel.ca>
2016-01-29 16:51:34 -0800

    Fix formatting

--- emacs/misc.el
+++ emacs/misc.el
@@ -44,11 +44,11 @@
 (setq temporary-file-directory "/tmp/emacs")
 
 (setq
-   backup-by-copying t      ; don't clobber symlinks
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)       ; use versioned backups
+ backup-by-copying t      ; don't clobber symlinks
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)       ; use versioned backups
 
 ;; Emacs package manager!
 (require 'package)
